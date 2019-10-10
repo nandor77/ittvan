f7567c61d5446f1424a10f8883ad86b0b2cd7d96
nandor.birher <nandor.birher@szt-w10-24.gimilabor.waldorf-obuda.lan>
2019-10-10 10:45:28 +0200

    vátozó létrehozva

--- src/ittvan/Ittvan.java
+++ src/ittvan/Ittvan.java
@@ -17,6 +17,11 @@ public class Ittvan {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("");  
+        int a=2;
+       int b=4;
+       int c=a+b;
+        System.out.println(c);
+        
     }
     
 }
